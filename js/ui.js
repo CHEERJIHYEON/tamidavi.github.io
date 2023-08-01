d19d97b929d3c62100c3cd1016fe8131e26a6b6e
JIHYEON <gimjihyeon111@gmail.com>
2023-08-01 19:13:36 +0900

    udemy 두번째 강의 실습 자료입니다.

---
+++ js/ui.js
@@ -0,0 +1,8 @@
+$(document).on('click', '#menuButton', function(){
+    $('#hamburgerMenu').show();
+})
+
+$(document).on('click', '#hamburgerCloseButton', function(){
+    $('#hamburgerMenu').hide();
+
+}) 
